e4ed3ae71a161447942c39f20aa3572b930481e2
MattCarrington <35834302+MattCarrington@users.noreply.github.com>
2025-08-12 21:34:35 +0100

    feat: add wiremock

---
+++ ThunderbirdsBoardGameEngine.GameData.Api.WireMock/Stubs/V1/DisasterCardStub.cs
@@ -0,0 +1,54 @@
+﻿using System.Net;
+using ThunderbirdsBoardGameEngine.GameData.Api.Contracts.Dtos.V1;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
+
+namespace ThunderbirdsBoardGameEngine.GameData.Api.WireMock.Stubs.V1
+{
+    public class DisasterCardStub
+    {
+        private readonly WireMockServer _wireMockServer;
+
+        public DisasterCardStub(WireMockServer wireMockServer)
+        {
+            _wireMockServer = wireMockServer;
+        }
+
+        public void GetAllAsyncSuccess(IReadOnlyList<DisasterCardDto> disasterCardDtos)
+        {
+            _wireMockServer.Given(Request.Create().WithPath("/api/DisasterCard").WithHeader("X-Api-Version", "1.0").UsingGet())
+                .RespondWith(Response.Create()
+                    .WithStatusCode(HttpStatusCode.OK)
+                    .WithHeader("Content-Type", "application/json")
+                    .WithBodyAsJson(disasterCardDtos));
+        }
+
+        public void GetByIdAsyncSuccess(DisasterCardDto disasterCardDto)
+        {
+            _wireMockServer.Given(Request.Create().WithPath($"/api/DisasterCard/{disasterCardDto.Id}").WithHeader("X-Api-Version", "1.0").UsingGet())
+                .RespondWith(Response.Create()
+                    .WithStatusCode(HttpStatusCode.OK)
+                    .WithHeader("Content-Type", "application/json")
+                    .WithBodyAsJson(disasterCardDto));
+        }
+
+        public void GetAllAsyncError(HttpStatusCode statusCode, string errorMessage)
+        {
+            _wireMockServer.Given(Request.Create().WithPath("/api/DisasterCard").WithHeader("X-Api-Version", "1.0").UsingGet())
+                .RespondWith(Response.Create()
+                    .WithStatusCode(statusCode)
+                    .WithHeader("Content-Type", "application/json")
+                    .WithBodyAsJson(new { Error = errorMessage }));
+        }
+        
+        public void GetByIdAsyncError(int id, HttpStatusCode statusCode, string errorMessage)
+        {
+            _wireMockServer.Given(Request.Create().WithPath($"/api/DisasterCard/{id}").WithHeader("X-Api-Version", "1.0").UsingGet())
+                .RespondWith(Response.Create()
+                    .WithStatusCode(statusCode)
+                    .WithHeader("Content-Type", "application/json")
+                    .WithBodyAsJson(new { Error = errorMessage }));
+        }
+    }
+}
---
+++ ThunderbirdsBoardGameEngine.GameData.Api.WireMock/ThunderbirdsGameDataApiWireMockServer.cs
@@ -0,0 +1,27 @@
+﻿using ThunderbirdsBoardGameEngine.GameData.Api.WireMock.Stubs.V1;
+using WireMock.Server;
+
+namespace ThunderbirdsBoardGameEngine.GameData.Api.WireMock
+{
+    internal class ThunderbirdsGameDataApiWireMockServer : IAsyncDisposable
+    {
+        private readonly WireMockServer _wireMockServer;
+
+        public DisasterCardStub DisasterCardStub { get; }
+
+        public string Url => _wireMockServer.Urls.FirstOrDefault();
+
+        public ThunderbirdsGameDataApiWireMockServer(int? port = null)
+        {
+            _wireMockServer = WireMockServer.Start(port ?? 5000);
+            DisasterCardStub = new DisasterCardStub(_wireMockServer);
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            _wireMockServer.Stop();
+            _wireMockServer.Dispose();
+            await Task.CompletedTask;
+        }
+    }
+}
