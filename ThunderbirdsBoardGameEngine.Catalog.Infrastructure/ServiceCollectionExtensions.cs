9a1546d0efd34a1a474f97bd051eb980741abce4
MattCarrington <35834302+MattCarrington@users.noreply.github.com>
2025-10-10 12:05:07 +0100

    feat: harden Validator

--- ThunderbirdsBoardGameEngine.Catalog.Infrastructure/ServiceCollectionExtensions.cs
+++ ThunderbirdsBoardGameEngine.Catalog.Infrastructure/ServiceCollectionExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
+using System.IO.Abstractions;
 using ThunderbirdsBoardGameEngine.Catalog.Application.Interfaces;
 using ThunderbirdsBoardGameEngine.Catalog.Infrastructure.Configuration;
 using ThunderbirdsBoardGameEngine.Catalog.Infrastructure.Interfaces;
@@ -15,15 +16,17 @@ namespace ThunderbirdsBoardGameEngine.Catalog.Infrastructure
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddSingleton<IFileReader, FileReader>();
+            services.AddSingleton<IFileSystem, FileSystem>();
+
+            services.AddSingleton<IPostConfigureOptions<DisasterCardJsonOptions>, DisasterCardJsonPostConfigure>();
+            services.AddSingleton<IValidateOptions<DisasterCardJsonOptions>, DisasterCardJsonOptionsValidator>();
+            services.AddScoped<IDisasterCardRepository, DisasterCardJsonRepository>();
+
             services.AddOptions<DisasterCardJsonOptions>()
                 .Bind(configuration.GetSection("Catalog:DisasterCards:Json"))
                 .ValidateOnStart(); // ← run all validators during startup
 
-            services.AddSingleton<IPostConfigureOptions<DisasterCardJsonOptions>, DisasterCardJsonPostConfigure>();
-            services.AddSingleton<IValidateOptions<DisasterCardJsonOptions>, DisasterCardJsonOptionsValidator>();
-            services.AddSingleton<IFileReader, FileReader>();
-
-            services.AddScoped<IDisasterCardRepository, DisasterCardJsonRepository>();
             return services;
         }
     }
