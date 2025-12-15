474d16e367738d86ca99f4b6a9ce2f31819fd3f4
MattCarrington <35834302+MattCarrington@users.noreply.github.com>
2025-12-15 17:23:47 +0000

    feat: implement bonus calculator

---
+++ ThunderbirdsBoardGameEngine.Rules.Application/Rescue/CalculateRescueTarget/BonusCalculationResult.cs
@@ -0,0 +1,9 @@
+﻿namespace ThunderbirdsBoardGameEngine.Rules.Application.Rescue.CalculateRescueTarget
+{
+    public record BonusCalculationResult
+    {
+        public required int TargetRoll { get; init; }
+
+        public required int TotalBonus { get; init; }
+    }
+}
---
+++ ThunderbirdsBoardGameEngine.Rules.Application/Rescue/CalculateRescueTarget/BonusCalculator.cs
@@ -0,0 +1,20 @@
+﻿namespace ThunderbirdsBoardGameEngine.Rules.Application.Rescue.CalculateRescueTarget
+{
+    public class BonusCalculator
+    {
+        public BonusCalculationResult CalculateRescueTarget(int difficultyNumber, IReadOnlyCollection<string> appliedBonusKeys, IReadOnlyCollection<RuleBonus> cardBonuses)
+        {
+            var appliedBonuses = cardBonuses
+                .Where(b => appliedBonusKeys.Contains(b.Key))
+                .ToList();
+
+            var bonus = appliedBonuses.Sum(b => b.Value);
+
+            return new BonusCalculationResult
+            {
+                TargetRoll = difficultyNumber - bonus,
+                TotalBonus = bonus
+            };
+        }            
+    }
+}
