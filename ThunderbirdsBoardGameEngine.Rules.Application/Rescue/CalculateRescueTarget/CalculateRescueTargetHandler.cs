667cd5bf58027a672f9e12ac5ab57a15b089ed5b
MattCarrington <35834302+MattCarrington@users.noreply.github.com>
2025-12-16 17:40:39 +0000

    feat: implement handler to orchestrate

---
+++ ThunderbirdsBoardGameEngine.Rules.Application/Rescue/CalculateRescueTarget/CalculateRescueTargetHandler.cs
@@ -0,0 +1,30 @@
+﻿using ThunderbirdsBoardGameEngine.Rules.Application.Interfaces;
+
+namespace ThunderbirdsBoardGameEngine.Rules.Application.Rescue.CalculateRescueTarget
+{
+    public class CalculateRescueTargetHandler
+    {
+        private readonly IRescueContextProvider _rescueContextProvider;
+        private readonly BonusCalculator _bonusCalculator;
+
+        public CalculateRescueTargetHandler(IRescueContextProvider rescueContextProvider, BonusCalculator bonusCalculator)
+        {
+            _rescueContextProvider = rescueContextProvider;
+            _bonusCalculator = bonusCalculator;
+        }
+
+        public CalculateRescueTargetResponse Handle(CalculateRescueTargetRequest request)
+        {
+            var context = _rescueContextProvider.GetRescueContext(request.DisasterCardId);
+
+            var calculatedTarget = _bonusCalculator.CalculateRescueTarget(request.AppliedBonusKeys, context);
+
+            return new CalculateRescueTargetResponse
+            (
+                TargetNumber: calculatedTarget.TargetRoll,
+                TotalBonus: calculatedTarget.TotalBonus,
+                AppliedBonuses: calculatedTarget.AppliedBonuses                    
+            );
+        }
+    }
+}
---
+++ ThunderbirdsBoardGameEngine.Rules.Application/Rescue/CalculateRescueTarget/CalculateRescueTargetResponse.cs
@@ -0,0 +1,4 @@
+﻿namespace ThunderbirdsBoardGameEngine.Rules.Application.Rescue.CalculateRescueTarget
+{
+    public record CalculateRescueTargetResponse(int TargetNumber, int TotalBonus, IReadOnlyList<RescueContextBonus> AppliedBonuses);
+}
