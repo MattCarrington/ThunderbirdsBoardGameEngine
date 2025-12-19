743ed789d80e80aa34f1a095f9d6e648b2ce1693
MattCarrington <35834302+MattCarrington@users.noreply.github.com>
2025-12-19 16:38:47 +0000

    feat: add extension method to register rescue rules

---
+++ ThunderbirdsBoardGameEngine.Rules.Infrastructure/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using ThunderbirdsBoardGameEngine.Rules.Application.Rescue.CalculateRescueTarget;
+using ThunderbirdsBoardGameEngine.Rules.Infrastructure.Providers;
+
+namespace ThunderbirdsBoardGameEngine.Rules.Infrastructure
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddRules(this IServiceCollection services)
+        {
+            services.AddMediatR(typeof(CalculateRescueTargetHandler).Assembly);
+
+            services.AddSingleton<RescueTargetCalculator>();
+            services.AddSingleton<IRescueProjectionProvider, CatalogRescueProjectionProvider>();
+
+            return services;
+        }
+    }
+}
