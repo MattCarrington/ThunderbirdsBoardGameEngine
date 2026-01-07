df4308145ba3fd2cb6401abc9d6f05725f12610a
MattCarrington <35834302+MattCarrington@users.noreply.github.com>
2026-01-07 15:56:43 +0000

    feat: create UI rescue service

---
+++ src/Ui/ThunderbirdsBoardGameEngine.UI/Services/RescueService.cs
@@ -0,0 +1,28 @@
+﻿using ThunderbirdsBoardGameEngine.Rules.Client.Interfaces.V1;
+using ThunderbirdsBoardGameEngine.Rules.Contracts.Dtos.Rescue.CalculateRescueTarget.V1;
+using ThunderbirdsBoardGameEngine.UI.Interfaces;
+
+namespace ThunderbirdsBoardGameEngine.UI.Services
+{
+    public class RescueService : IRescueService
+    {
+        private readonly IRescueClient _rescueClient;
+
+        public RescueService(IRescueClient rescueClient)
+        {
+            _rescueClient = rescueClient;
+        }
+
+        public async Task<CalculateRescueTargetResponseDto?> CalculateRescueTargetAsync(string disasterCardCode, IReadOnlyCollection<string> presentBonusKeys)
+        {
+            var request = new CalculateRescueTargetRequestDto
+            {
+                PresentDisasterBonusKeys = presentBonusKeys
+            };
+
+            var result = await _rescueClient.CalculateRescueTargetAsync(disasterCardCode, request);
+
+            return result.Success ? result.Data : null;
+        }
+    }
+}
---
+++ tests/Ui/UnitTests/ThunderbirdsBoardGameEngine.UI.UnitTests/Services/RescueServiceTests.cs
@@ -0,0 +1,93 @@
+﻿using NSubstitute;
+using System.Net;
+using ThunderbirdsBoardGameEngine.Client.Infrastructure;
+using ThunderbirdsBoardGameEngine.Rules.Client.Interfaces.V1;
+using ThunderbirdsBoardGameEngine.Rules.Contracts.Dtos.Rescue.CalculateRescueTarget.V1;
+using ThunderbirdsBoardGameEngine.UI.Services;
+using Xunit;
+
+namespace ThunderbirdsBoardGameEngine.UI.UnitTests.Services
+{
+    public class RescueServiceTests
+    {
+        [Fact]
+        public async Task CalculateRescueTargetAsync_WhenResponseIsSuccessful_ReturnsCalculateRescueTargetResponseDto()
+        {
+            // Arrange
+            var (disasterCardCode, presentBonusKeys) = CreateRequestParameters();
+
+            var expectedResponse = new CalculateRescueTargetResponseDto
+            {
+                TargetNumber = 10,
+                TotalBonus = 5,
+                AppliedDisasterBonuses = []
+            };
+
+            var apiResult = ApiResult<CalculateRescueTargetResponseDto>.SuccessResult(expectedResponse, HttpStatusCode.OK);
+            
+            var rescueClient = CreateRescueClient(apiResult);
+
+            var service = CreateRescueService(rescueClient);
+
+            // Act
+            var result = await service.CalculateRescueTargetAsync(disasterCardCode, presentBonusKeys);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(expectedResponse, result);
+
+            var expectedRequest = new CalculateRescueTargetRequestDto
+            {
+                PresentDisasterBonusKeys = presentBonusKeys
+            };
+
+            await rescueClient.Received(1)
+                .CalculateRescueTargetAsync(
+                    Arg.Is(disasterCardCode),
+                    Arg.Is(expectedRequest)
+                );
+        }
+
+        [Fact]
+        public async Task CalculateRescueTargetAsync_WhenResponseIsNotSuccessful_ReturnsNull()
+        {
+            // Arrange
+            var (disasterCardCode, presentBonusKeys) = CreateRequestParameters();
+
+            var apiResult = ApiResult<CalculateRescueTargetResponseDto>.Failure("Error", HttpStatusCode.BadRequest);
+            
+            var rescueClient = CreateRescueClient(apiResult);
+
+            var service = CreateRescueService(rescueClient);
+
+            // Act
+            var result = await service.CalculateRescueTargetAsync(disasterCardCode, presentBonusKeys);
+
+            // Assert
+            Assert.Null(result);
+        }
+
+        private static (string, IReadOnlyCollection<string>) CreateRequestParameters()
+        {
+            var disasterCardCode = "DISASTER_001";
+
+            var presentBonusKeys = new[] { "BONUS_001", "BONUS_002" };
+
+            return (disasterCardCode, presentBonusKeys);
+        }
+
+        private static IRescueClient CreateRescueClient(ApiResult<CalculateRescueTargetResponseDto> apiResult)
+        {
+            var rescueClient = Substitute.For<IRescueClient>();
+            rescueClient.CalculateRescueTargetAsync(Arg.Any<string>(), Arg.Any<CalculateRescueTargetRequestDto>())
+                .Returns(apiResult);
+
+            return rescueClient;
+        }
+
+        private static RescueService CreateRescueService(IRescueClient rescueClient)
+        {
+            return new RescueService(rescueClient);
+        }
+    }
+}
