3570fe461f9f68091fbce5a09c8187e8d64318e8
MattCarrington <35834302+MattCarrington@users.noreply.github.com>
2026-01-19 10:42:55 +0000

    feat: implement character validator

---
+++ src/Catalog/ThunderbirdsBoardGameEngine.Catalog.Domain/Exceptions/CharacterDefinitionValidationException.cs
@@ -0,0 +1,59 @@
+﻿
+using ThunderbirdsBoardGameEngine.Catalog.Domain.Enums;
+
+namespace ThunderbirdsBoardGameEngine.Catalog.Domain.Exceptions
+{
+    public class CharacterDefinitionValidationException : DomainValidationException
+    {
+        public CharacterDefinitionErrorCode ErrorCode { get; }
+
+        public Character? Character { get; }
+
+        private CharacterDefinitionValidationException(
+            CharacterDefinitionErrorCode errorCode,
+            string message,
+            Character? character = null,
+            Exception? innerException = null) : base(message, innerException)
+        {
+            ErrorCode = errorCode;
+            Character = character;
+        }
+
+        public static CharacterDefinitionValidationException Unknown()
+        {
+            return new CharacterDefinitionValidationException(
+                CharacterDefinitionErrorCode.Unknown,
+                "An unknown character definition validation error has occurred.");
+        }
+
+        public static CharacterDefinitionValidationException NullEntry()
+        {
+            return new CharacterDefinitionValidationException(
+                CharacterDefinitionErrorCode.NullEntry,
+                "The character definitions collection contains a null entry.");
+        }
+
+        public static CharacterDefinitionValidationException DuplicateKey(Character character)
+        {
+            return new CharacterDefinitionValidationException(
+                CharacterDefinitionErrorCode.DuplicateKey,
+                $"A character definition with the key '{character}' already exists.",
+                character);
+        }
+
+        public static CharacterDefinitionValidationException InvalidCount()
+        {
+            return new CharacterDefinitionValidationException(
+                CharacterDefinitionErrorCode.InvalidCount,
+                $"The number of character definitions must be exactly six.");
+        }
+    }
+
+    public enum CharacterDefinitionErrorCode
+    {
+        Unknown = 0,
+        NullEntry,
+        DuplicateKey,
+        InvalidCount
+    }
+}
---
+++ tests/Catalog/UnitTests/ThunderbirdsBoardGameEngine.Catalog.Domain.UnitTests/Validators/CharacterDefinitionValidatorTests.cs
@@ -0,0 +1,105 @@
+﻿using ThunderbirdsBoardGameEngine.Catalog.Domain.Entities;
+using ThunderbirdsBoardGameEngine.Catalog.Domain.Enums;
+using ThunderbirdsBoardGameEngine.Catalog.Domain.Exceptions;
+using ThunderbirdsBoardGameEngine.Catalog.Domain.Validators;
+using Xunit;
+
+namespace ThunderbirdsBoardGameEngine.Catalog.Domain.UnitTests.Validators
+{
+    public class CharacterDefinitionValidatorTests
+    {
+        [Fact]
+        public void ValidateAll_WhenCharacterDefinitionsAreValid_DoesNotThrow()
+        {
+            // Arrange
+            var characters = new List<CharacterDefinition>
+            {
+                new(Character.Scott, new(RescueType.Air, 2)),
+                new(Character.Virgil, new(RescueType.Land, 2)),
+                new(Character.John, new(RescueType.Space, 2)),
+                new(Character.Gordon, new(RescueType.Sea, 3)),
+                new(Character.Alan, new(RescueType.Space, 2)),
+                new(Character.LadyPenelope)
+            };
+
+            // Act
+            var exception = Record.Exception(() => CharacterDefinitionValidator.ValidateAll(characters));
+
+            // Assert
+            Assert.Null(exception);
+        }
+
+        [Fact]
+        public void ValidateAll_WhenDuplicateKeys_ThrowsValidationException()
+        {
+            var duplicateCharacter = Character.Scott;
+
+            // Arrange
+            var characters = new List<CharacterDefinition>
+            {
+                new(duplicateCharacter),
+                new(duplicateCharacter, new(RescueType.Air, 2)),
+                new(Character.Virgil, new(RescueType.Land, 2)),
+                new(Character.John, new(RescueType.Space, 2)),
+                new(Character.Gordon, new(RescueType.Sea, 3)),
+                new(Character.Alan, new(RescueType.Space, 2)),
+            };
+            
+            // Act & Assert
+            var exception = Assert.Throws<CharacterDefinitionValidationException>(() => CharacterDefinitionValidator.ValidateAll(characters));
+            Assert.Contains($"A character definition with the key '{duplicateCharacter}' already exists.", exception.Message);
+            Assert.Equal(CharacterDefinitionErrorCode.DuplicateKey, exception.ErrorCode);
+            Assert.Equal(duplicateCharacter, exception.Character);
+        }
+
+        [Fact]
+        public void ValidateAll_WhenCharacterDefiinitionsLessThanSix_ThrowsValidationException()
+        {
+            // Arrange
+            var characters = new List<CharacterDefinition>
+            {
+                new(Character.Scott, new(RescueType.Air, 2)),
+                new(Character.Virgil, new(RescueType.Land, 2)),
+                new(Character.John, new(RescueType.Space, 2)),
+                new(Character.Gordon, new(RescueType.Sea, 3)),
+                new(Character.Alan, new(RescueType.Space, 2))
+            };
+
+            // Act & Assert
+            var exception = Assert.Throws<CharacterDefinitionValidationException>(() => CharacterDefinitionValidator.ValidateAll(characters));
+            Assert.Contains("The number of character definitions must be exactly six.", exception.Message);
+        }
+
+
+
+        [Fact]
+        public void ValidateAll_WhenCharacterDefiinitionsGreaterThanSix_ThrowsValidationException()
+        {
+            // Arrange
+            var characters = new List<CharacterDefinition>
+            {
+                new(Character.Scott, new(RescueType.Air, 2)),
+                new(Character.Virgil, new(RescueType.Land, 2)),
+                new(Character.John, new(RescueType.Space, 2)),
+                new(Character.Gordon, new(RescueType.Sea, 3)),
+                new(Character.Alan, new(RescueType.Space, 2)),
+                new(Character.LadyPenelope),
+                new(Character.LadyPenelope),
+            };
+
+            // Act & Assert
+            var exception = Assert.Throws<CharacterDefinitionValidationException>(() => CharacterDefinitionValidator.ValidateAll(characters));
+            Assert.Contains("The number of character definitions must be exactly six.", exception.Message);
+        }
+
+        [Fact]
+        public void ValidateAll_WhenCharacterDefinitionsAreNull_ThrowsArgumentNullException()
+        {
+            // Arrange
+            
+            // Act & Assert
+            var exception = Assert.Throws<ArgumentNullException>(() => CharacterDefinitionValidator.ValidateAll(null!));
+            Assert.Contains("Value cannot be null.", exception.Message);
+        }
+    }
+}
