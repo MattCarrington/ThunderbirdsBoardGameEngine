1aeae04ceeb6799adb0b45cc2b76e7efa1e47452
MattCarrington <35834302+MattCarrington@users.noreply.github.com>
2026-01-21 17:03:06 +0000

    feat: add service seam for API layer

---
+++ src/Catalog/ThunderbirdsBoardGameEngine.Catalog.Application/Interfaces/ICharacterDefinitionService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Immutable;
+using ThunderbirdsBoardGameEngine.Catalog.Domain.Entities;
+
+namespace ThunderbirdsBoardGameEngine.Catalog.Application.Interfaces
+{
+    public interface ICharacterDefinitionService
+    {
+        ImmutableArray<CharacterDefinition> GetAll();
+    }
+}
---
+++ src/Catalog/ThunderbirdsBoardGameEngine.Catalog.Application/Services/CharacterDefinitionService.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Immutable;
+using ThunderbirdsBoardGameEngine.Catalog.Application.Interfaces;
+using ThunderbirdsBoardGameEngine.Catalog.Domain.Entities;
+
+namespace ThunderbirdsBoardGameEngine.Catalog.Application.Services
+{
+    internal class CharacterDefinitionService : ICharacterDefinitionService
+    {
+        private readonly ICharacterReferenceSource _characterReferenceSource;
+
+        public CharacterDefinitionService(ICharacterReferenceSource characterReferenceSource)
+        {
+            _characterReferenceSource = characterReferenceSource ?? throw new ArgumentNullException(nameof(characterReferenceSource));
+        }
+
+        public ImmutableArray<CharacterDefinition> GetAll()
+        {
+            return _characterReferenceSource.Characters;
+        }
+    }
+}
