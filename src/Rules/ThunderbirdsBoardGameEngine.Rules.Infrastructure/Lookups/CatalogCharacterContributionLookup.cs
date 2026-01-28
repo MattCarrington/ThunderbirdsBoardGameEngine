adef4d9d1524be025332275c81a57ed81f830be4
MattCarrington <35834302+MattCarrington@users.noreply.github.com>
2026-01-28 20:59:58 +0000

    feat: add look up to Character reference source

---
+++ src/Rules/ThunderbirdsBoardGameEngine.Rules.Infrastructure/Lookups/CatalogCharacterContributionLookup.cs
@@ -0,0 +1,33 @@
+﻿using ThunderbirdsBoardGameEngine.Catalog.Application.Interfaces;
+using ThunderbirdsBoardGameEngine.Catalog.Application.Mappers;
+using ThunderbirdsBoardGameEngine.PublishedLanguage.Characters;
+using ThunderbirdsBoardGameEngine.Rules.Application.Rescue.Interfaces;
+using ThunderbirdsBoardGameEngine.Rules.Domain.Rescue;
+
+namespace ThunderbirdsBoardGameEngine.Rules.Infrastructure.Lookups
+{
+    internal class CatalogCharacterContributionLookup : ICharacterContributionLookup
+    {
+        private readonly ICharacterDefinitionReferenceSource _characterDefinitionReferenceSource;
+
+        public CatalogCharacterContributionLookup(ICharacterDefinitionReferenceSource characterDefinitionReferenceSource)
+        {
+            _characterDefinitionReferenceSource = characterDefinitionReferenceSource;
+        }
+
+        public CharacterContribution GetCharacterContribution(CharacterCode characterCode)
+        {
+            var character = CharacterCodeMapper.ToDomain(characterCode);
+
+            var definition = _characterDefinitionReferenceSource.GetCharacterDefinition(character);
+
+            return new CharacterContribution
+            {
+                Key = characterCode,
+                RescueBonusContribution = definition.RescueBonus is null
+                    ? null
+                    : new CharacterRescueBonusContribution(definition.RescueBonus.RescueType, definition.RescueBonus.BonusValue)
+            };
+        }
+    }
+}
