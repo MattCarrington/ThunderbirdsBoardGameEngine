e9a9902636dc8475f5817b1eda0c3ff739e2f94e
MattCarrington <35834302+MattCarrington@users.noreply.github.com>
2026-01-31 17:30:33 +0000

    tests: clearer name as invalid could mean many things

--- tests/Rules/UnitTests/ThunderbirdsBoardGameEngine.Rules.Domain.UnitTests/Rescue/DisasterContributionTests.cs
+++ tests/Rules/UnitTests/ThunderbirdsBoardGameEngine.Rules.Domain.UnitTests/Rescue/DisasterContributionTests.cs
@@ -60,7 +60,7 @@ namespace ThunderbirdsBoardGameEngine.Rules.Domain.UnitTests.Rescue
         }
 
         [Fact]
-        public void ApplyRescueModifier_WhenInvalidBonusKey_ReturnsEmpty()
+        public void ApplyRescueModifier_WhenBonusKeyDonNotExistOnCard_ReturnsEmpty()
         {
             // Arrange
             var contribution = CreateDisasterContribution();
