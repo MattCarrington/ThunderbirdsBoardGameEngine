75bea5c9df90e2c840245d4496cec94721a6be08
MattCarrington <35834302+MattCarrington@users.noreply.github.com>
2026-02-01 16:34:52 +0000

    feat: add UI service for fetching data

---
+++ src/Ui/ThunderbirdsBoardGameEngine.UI/Services/CharactersService.cs
@@ -0,0 +1,27 @@
+﻿using ThunderbirdsBoardGameEngine.Catalog.Client.Interfaces.V1;
+using ThunderbirdsBoardGameEngine.Catalog.Contracts.Dtos.V1;
+
+namespace ThunderbirdsBoardGameEngine.UI.Services
+{
+    public class CharactersService
+    {
+        private readonly ICharactersClient _client;
+
+        public CharactersService(ICharactersClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<IReadOnlyList<CharacterDto>> GetAllAsync()
+        {
+            var result = await _client.GetAllAsync();
+
+            if (result.Success && result.Data is not null)
+            {
+                return result.Data;
+            }
+
+            return []; // Return an empty list if the request fails
+        }
+    }
+}
