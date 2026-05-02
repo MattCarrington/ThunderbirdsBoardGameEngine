7d9df8580cc9a8a456e1727a4046fd97712a69cd
MattCarrington <35834302+MattCarrington@users.noreply.github.com>
2026-05-02 16:34:47 +0100

    tests: add unit test for new identities

---
+++ tests/ReferenceData/UnitTests/ThunderbirdsBoardGameEngine.ReferenceData.UnitTests/Identities/PodVehicleCodeTests.cs
@@ -0,0 +1,33 @@
+﻿using ThunderbirdsBoardGameEngine.ReferenceData.Identities;
+using ThunderbirdsBoardGameEngine.TestUtils.xUnit.ClassData;
+using Xunit;
+
+namespace ThunderbirdsBoardGameEngine.ReferenceData.UnitTests.Identities
+{
+    public class PodVehicleCodeTests
+    {
+        [Fact]
+        public void Constructor_WhenValidCode_CreatesInstance()
+        {
+            // Arrange
+            var code = "valid-pod-vehicle-code";
+
+            // Act
+            var result = new PodVehicleCode(code);
+
+            // Assert
+            Assert.Equal(code, result.Value);
+            Assert.Equal(code, result.ToString());
+        }
+
+        [Theory]
+        [ClassData(typeof(NullOrWhitespaceStringData))]
+        public void Constructor_WhenInvalidCardValues_ThrowsArgumentException(string code)
+        {
+            // Arrange
+
+            // Act & Assert
+            Assert.ThrowsAny<ArgumentException>(() => new PodVehicleCode(code));
+        }
+    }
+}
