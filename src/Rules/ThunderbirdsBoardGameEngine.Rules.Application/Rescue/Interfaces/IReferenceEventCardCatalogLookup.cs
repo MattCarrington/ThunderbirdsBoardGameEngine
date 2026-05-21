1f054df6c4e59108e2f5ed2d16f3cf4ac546b455
MattCarrington <35834302+MattCarrington@users.noreply.github.com>
2026-05-21 18:03:55 +0100

    feat: create lookups for event/fab cards

---
+++ src/Rules/ThunderbirdsBoardGameEngine.Rules.Application/Rescue/Interfaces/IReferenceEventCardCatalogLookup.cs
@@ -0,0 +1,9 @@
+﻿using ThunderbirdsBoardGameEngine.ReferenceData.Identities;
+
+namespace ThunderbirdsBoardGameEngine.Rules.Application.Rescue.Interfaces
+{
+    public interface IReferenceEventCardCatalogLookup
+    {
+        bool Exists(CardCode cardCode);
+    }
+}
