cee43723626f2413d2ef2c0ec21317e753fe34c7
MattCarrington <35834302+MattCarrington@users.noreply.github.com>
2026-06-03 18:12:20 +0100

    feat: add action point calculator

---
+++ src/Rules/ThunderbirdsBoardGameEngine.Rules.Domain/Movement/ActionPointCalculator.cs
@@ -0,0 +1,20 @@
+﻿namespace ThunderbirdsBoardGameEngine.Rules.Domain.Movement
+{
+    public class ActionPointCalculator
+    {
+        public int CalculateActionPoints(int spacesTravelled, int topSpeed)
+        {
+            if (spacesTravelled < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(spacesTravelled), "Spaces travelled cannot be negative.");
+            }
+
+            if (topSpeed <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(topSpeed), "Top Speed cannot be zero or negative");
+            }
+
+            return (int)Math.Ceiling((decimal)spacesTravelled / topSpeed);
+        }
+    }
+}
