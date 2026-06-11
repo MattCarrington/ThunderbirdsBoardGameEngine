7ef7342f06ce58e25a4de9f3db6232d1dcc97fe4
MattCarrington <35834302+MattCarrington@users.noreply.github.com>
2026-06-11 19:14:28 +0100

    tests: undo test change

--- tests/Common/ThunderbirdsBoardGameEngine.Client.Infrastructure.UnitTests/Handlers/DefaultHttpHandlerTests.cs
+++ tests/Common/ThunderbirdsBoardGameEngine.Client.Infrastructure.UnitTests/Handlers/DefaultHttpHandlerTests.cs
@@ -85,7 +85,7 @@ namespace ThunderbirdsBoardGameEngine.Client.Infrastructure.UnitTests.Handlers
 
             using var response = new HttpResponseMessage(HttpStatusCode.BadRequest)
             {
-                Content = new StringContent("Bad Request Error Messages")
+                Content = new StringContent("Bad Request Error Message")
             };
 
             // Act
@@ -97,7 +97,7 @@ namespace ThunderbirdsBoardGameEngine.Client.Infrastructure.UnitTests.Handlers
             Assert.Equal(HttpStatusCode.BadRequest, result.StatusCode);
             Assert.Null(result.Data);
             Assert.NotNull(result.ErrorMessage);
-            Assert.Equal("Bad Request Error Messages", result.ErrorMessage);
+            Assert.Equal("Bad Request Error Message", result.ErrorMessage);
         }
 
         [Fact]
