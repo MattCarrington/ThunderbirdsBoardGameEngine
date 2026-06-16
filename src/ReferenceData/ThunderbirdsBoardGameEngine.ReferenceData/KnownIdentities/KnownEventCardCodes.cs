c44a9c1e589cf7784c1e93279bca5bfd7bfebbf8
MattCarrington <35834302+MattCarrington@users.noreply.github.com>
2026-06-16 16:12:04 +0100

    feat: define known codes

---
+++ src/ReferenceData/ThunderbirdsBoardGameEngine.ReferenceData/KnownIdentities/KnownEventCardCodes.cs
@@ -0,0 +1,13 @@
+﻿using ThunderbirdsBoardGameEngine.ReferenceData.Identities;
+
+namespace ThunderbirdsBoardGameEngine.ReferenceData.KnownIdentities
+{
+    public static class KnownEventCardCodes
+    {
+        public static readonly CardCode AttackOfTheZombites = new("attack-of-the-zombites");
+
+        public static readonly CardCode UsnSentinelMissileAttack = new("usn-sentinel-missile-attack");
+
+        public static readonly CardCode RocketMalfunction = new("rocket-malfunction");
+    }
+}
