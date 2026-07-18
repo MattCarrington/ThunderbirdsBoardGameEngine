26433def2df9023b47c9b1eaaf8fe6bdb2347218
MattCarrington <35834302+MattCarrington@users.noreply.github.com>
2026-07-18 18:10:13 +0100

    refactor: extract the disaster card building to a mapper (#108)

---
+++ tools/ReferenceData/ThunderbirdsBoardGameEngine.ReferenceData.Compiler/Mapper/DisasterCardMapper.cs
@@ -0,0 +1,62 @@
+﻿using ThunderbirdsBoardGameEngine.ReferenceData.Compiler.Helpers;
+using ThunderbirdsBoardGameEngine.ReferenceData.Compiler.Inputs;
+using ThunderbirdsBoardGameEngine.ReferenceData.Compiler.Resolvers;
+using ThunderbirdsBoardGameEngine.ReferenceData.Core.Enums;
+using ThunderbirdsBoardGameEngine.ReferenceData.Core.Identities;
+using ThunderbirdsBoardGameEngine.ReferenceData.Core.Model;
+
+namespace ThunderbirdsBoardGameEngine.ReferenceData.Compiler.Mapper
+{
+    public class DisasterCardMapper
+    {
+        private readonly LocationCodeResolver _locationCodeResolver;
+        private readonly DisasterBonusTargetResolver _disasterBonusTargetResolver;
+
+        public DisasterCardMapper(LocationCodeResolver locationCodeResolver, DisasterBonusTargetResolver disasterBonusTargetResolver)
+        {
+            _locationCodeResolver = locationCodeResolver;
+            _disasterBonusTargetResolver = disasterBonusTargetResolver;
+        }
+
+        public IEnumerable<ReferenceDisasterDefinition> Map(
+            IEnumerable<DisasterInput> inputs)
+        {
+            return inputs
+                .Select(MapDisaster);
+
+        }
+
+        private ReferenceDisasterDefinition MapDisaster(DisasterInput input)
+        {
+            return new ReferenceDisasterDefinition(
+                new CardCode(StringHelpers.Slugify(input.Name)),
+                StringHelpers.NormalizeWhitespace(input.Name, nameof(input.Name)),
+                input.DifficultyNumber,
+                _locationCodeResolver.Resolve(input.Location),
+                Enum.Parse<RescueType>(input.RescueType, true),
+                MapBonuses(input.Bonuses).ToList(),
+                MapRewards(input.Rewards).ToList());
+        }
+
+        private IEnumerable<ReferenceDisasterBonus> MapBonuses(IEnumerable<BonusInput> bonuses)
+        {
+            return bonuses.Select(b =>
+                new ReferenceDisasterBonus(
+                    _disasterBonusTargetResolver.Resolve(b.TargetName),
+                    b.Value,
+                    b.Location is null ? null : _locationCodeResolver.Resolve(b.Location)
+                )
+            );
+        }
+
+        private static IEnumerable<ReferenceDisasterReward> MapRewards(IEnumerable<string> rewards)
+        {
+            return rewards.Select(r =>
+                r.Equals("User Choice", StringComparison.OrdinalIgnoreCase)
+                    ? (ReferenceDisasterReward)new ReferenceDisasterReward.PlayerChoice()
+                    : new ReferenceDisasterReward.SpecificToken(
+                        Enum.Parse<BonusToken>(r, true))
+            );
+        }
+    }
+}
