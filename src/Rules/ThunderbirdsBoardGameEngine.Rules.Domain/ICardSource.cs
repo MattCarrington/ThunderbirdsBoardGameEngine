c125e2493430c810cd020d876b29f86ee4d605bb
MattCarrington <35834302+MattCarrington@users.noreply.github.com>
2026-07-23 18:23:28 +0100

    feat: implement card source interface

---
+++ src/Rules/ThunderbirdsBoardGameEngine.Rules.Domain/ICardSource.cs
@@ -0,0 +1,9 @@
+﻿using ThunderbirdsBoardGameEngine.ReferenceData.Core.Identities;
+
+namespace ThunderbirdsBoardGameEngine.Rules.Domain
+{
+    public interface ICardSource
+    {
+        CardCode CardCode { get; }
+    }
+}
