9add9a748b72ffd6208d4062679af6953760efda
MattCarrington <35834302+MattCarrington@users.noreply.github.com>
2026-07-25 14:22:00 +0100

    feat: define prominent locations and characters

---
+++ src/ReferenceData/ThunderbirdsBoardGameEngine.ReferenceData.Core/KnownIdentities/KnownCharacterCodes.cs
@@ -0,0 +1,19 @@
+﻿using ThunderbirdsBoardGameEngine.ReferenceData.Core.Identities;
+
+namespace ThunderbirdsBoardGameEngine.ReferenceData.Core.KnownIdentities
+{
+    public static class KnownCharacterCodes
+    {
+        public static readonly CharacterCode Scott = new("scott");
+
+        public static readonly CharacterCode Virgil = new("virgil");
+
+        public static readonly CharacterCode John = new("john");
+
+        public static readonly CharacterCode Gordon = new("gordon");
+
+        public static readonly CharacterCode Alan = new("alan");
+
+        public static readonly CharacterCode LadyPenelope = new("lady-penelope");
+    }
+}
---
+++ src/ReferenceData/ThunderbirdsBoardGameEngine.ReferenceData.Core/KnownIdentities/KnownLocationCodes.cs
@@ -0,0 +1,13 @@
+﻿using ThunderbirdsBoardGameEngine.ReferenceData.Core.Identities;
+
+namespace ThunderbirdsBoardGameEngine.ReferenceData.Core.KnownIdentities
+{
+    public static class KnownLocationCodes
+    {
+        public static readonly LocationCode SouthPacific = new("south-pacific");
+
+        public static readonly LocationCode Europe = new("europe");
+
+        public static readonly LocationCode GeoStationaryOrbit = new("geo-stationary-orbit");
+    }
+}
