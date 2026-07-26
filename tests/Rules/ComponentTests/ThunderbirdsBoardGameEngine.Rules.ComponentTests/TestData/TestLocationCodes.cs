8183df1b8b35b41e72f9062c66bd3a60acfc1d30
MattCarrington <35834302+MattCarrington@users.noreply.github.com>
2026-07-26 15:34:54 +0100

    chore: use properties

--- tests/Rules/ComponentTests/ThunderbirdsBoardGameEngine.Rules.ComponentTests/TestData/TestLocationCodes.cs
+++ tests/Rules/ComponentTests/ThunderbirdsBoardGameEngine.Rules.ComponentTests/TestData/TestLocationCodes.cs
@@ -4,26 +4,26 @@ namespace ThunderbirdsBoardGameEngine.Rules.ComponentTests.TestData
 {
     public static class TestLocationCodes
     {
-        public static readonly LocationCode Europe = new("europe");
+        public static LocationCode Europe => new("europe");
 
-        public static readonly LocationCode Asia = new("asia");
+        public static LocationCode Asia => new("asia");
 
-        public static readonly LocationCode Africa = new("africa");
+        public static LocationCode Africa => new("africa");
 
-        public static readonly LocationCode Australia = new("australia");
+        public static LocationCode Australia => new("australia");
 
-        public static readonly LocationCode NorthAmerica = new("north-america");
+        public static LocationCode NorthAmerica => new("north-america");
 
-        public static readonly LocationCode SouthAmerica = new("south-america");
+        public static LocationCode SouthAmerica => new("south-america");
 
-        public static readonly LocationCode NorthAtlantic = new("north-atlantic");
+        public static LocationCode NorthAtlantic => new("north-atlantic");
 
-        public static readonly LocationCode Pacific = new("pacific");
+        public static LocationCode Pacific => new("pacific");
 
-        public static readonly LocationCode Space = new("space");
+        public static LocationCode Space => new("space");
 
-        public static readonly LocationCode Moon = new("moon");
+        public static LocationCode Moon => new("moon");
 
-        public static readonly LocationCode Sun = new("sun");
+        public static LocationCode Sun => new("sun");
     }
 }
