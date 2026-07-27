71c63a01ea89a37aa05533f387a833e6f289002d
MattCarrington <35834302+MattCarrington@users.noreply.github.com>
2026-07-27 13:09:13 +0100

    feat: register DI

---
+++ src/GameState/ThunderbirdsBoardGameEngine.GameState.Infrastructure/GameStatePersistenceOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace ThunderbirdsBoardGameEngine.GameState.Infrastructure
+{
+    public sealed class GameStatePersistenceOptions
+    {
+        public const string SectionName = "GameState:Persistence";
+
+        public string ConnectionString { get; set; } = string.Empty;
+    }
+}
---
+++ src/GameState/ThunderbirdsBoardGameEngine.GameState.Infrastructure/ServiceCollectionExtensions.cs
@@ -0,0 +1,44 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using ThunderbirdsBoardGameEngine.GameState.Application;
+using ThunderbirdsBoardGameEngine.GameState.Application.CreateGame;
+using ThunderbirdsBoardGameEngine.GameState.Domain.Setup.V1;
+using ThunderbirdsBoardGameEngine.GameState.Infrastructure.GameStateIntegrity;
+using ThunderbirdsBoardGameEngine.GameState.Infrastructure.Persistence;
+
+namespace ThunderbirdsBoardGameEngine.GameState.Infrastructure
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddGameState(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddOptions<GameStatePersistenceOptions>()
+                .Bind(configuration.GetSection(GameStatePersistenceOptions.SectionName));
+
+            services.AddMediatR(typeof(CreateNewGameHandler).Assembly);
+
+            services.AddSingleton<IGameStateIntegrityValidator, ReferenceDataGameStateIntegrityValidator>();
+
+            services.AddDbContext<GameStateDbContext>((serviceProvider, options) =>
+            {
+                var persistenceOptions = serviceProvider.GetRequiredService<IOptions<GameStatePersistenceOptions>>().Value;
+                options.UseNpgsql(persistenceOptions.ConnectionString);
+            });
+
+            services.AddScoped<IGameRepository, GameRepository>();
+            services.AddSingleton<GameRecordMapper>();
+
+            RegisterCreateGame(services);
+
+            return services;
+        }
+
+        private static void RegisterCreateGame(IServiceCollection services)
+        {
+            services.AddSingleton<StandardGameSetupFactory>();
+        }
+    }
+}
