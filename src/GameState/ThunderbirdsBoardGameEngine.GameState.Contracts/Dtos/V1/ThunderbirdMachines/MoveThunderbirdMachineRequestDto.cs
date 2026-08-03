cfe1581c9fc1a38208baac6b6770fd7cf03fb96f
MattCarrington <35834302+MattCarrington@users.noreply.github.com>
2026-08-03 17:19:49 +0100

    chore: make destination required

--- src/GameState/ThunderbirdsBoardGameEngine.GameState.Contracts/Dtos/V1/ThunderbirdMachines/MoveThunderbirdMachineRequestDto.cs
+++ src/GameState/ThunderbirdsBoardGameEngine.GameState.Contracts/Dtos/V1/ThunderbirdMachines/MoveThunderbirdMachineRequestDto.cs
@@ -2,6 +2,6 @@
 {
     public record MoveThunderbirdMachineRequestDto
     {
-        public string Destination { get; init; } = string.Empty;
+        public required string Destination { get; init; }
     }
 }
